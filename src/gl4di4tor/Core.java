04f445685cc44ce85f69e85bc6d90069ea6e452e
arVahedi <ar.vahedi357@gmail.com>
2017-07-28 03:45:57 +0430

    Add JCDP library for log service.
    Signed-off-by: arVahedi <ar.vahedi357@gmail.com>

--- src/gl4di4tor/Core.java
+++ src/gl4di4tor/Core.java
@@ -1,5 +1,7 @@
 package gl4di4tor;
 
+import com.diogonunes.jcdp.color.ColoredPrinter;
+import com.diogonunes.jcdp.color.api.Ansi;
 import gl4di4tor.configuration.Config;
 import gl4di4tor.engine.proxy.ProxyEngine;
 
@@ -11,6 +13,34 @@ public class Core {
         try {
             Thread t = new ProxyEngine(Config.getInstance().getProxyServerPort());
             t.start();
+
+
+            ///////////////////////////////////////
+            ColoredPrinter cp = new ColoredPrinter.Builder(1, false)
+                    .foreground(Ansi.FColor.WHITE).background(Ansi.BColor.BLUE)   //setting format
+                    .build();
+
+            //printing according to that format
+            cp.println(cp);
+            cp.setAttribute(Ansi.Attribute.REVERSE);
+            cp.println("This is a normal message (with format reversed).");
+
+            //resetting the terminal to its default colors
+            cp.clear();
+            cp.print(cp.getDateFormatted(), Ansi.Attribute.NONE, Ansi.FColor.CYAN, Ansi.BColor.BLACK);
+            cp.debugPrintln(" This debug message is always printed.");
+            cp.clear();
+            cp.print("This example used JCDP 1.25   ");
+
+            //temporarily overriding that format
+            cp.print("\tMADE ", Ansi.Attribute.BOLD, Ansi.FColor.YELLOW, Ansi.BColor.GREEN);
+            cp.print("IN PORTUGAL\t\n", Ansi.Attribute.BOLD, Ansi.FColor.RED, Ansi.BColor.NONE);
+            cp.println("I hope you find it useful ;)");
+
+            cp.clear(); //don't forget to clear the terminal's format before exiting
+            ///////////////////////////////////////
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
---
+++ src/gl4di4tor/log/LogService.java
@@ -0,0 +1,7 @@
+package gl4di4tor.log;
+
+/**
+ * Created by Gladiator on 7/28/2017 AD.
+ */
+public class LogService {
+}
