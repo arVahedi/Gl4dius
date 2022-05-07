e5cdc040033e3eac72211f7bc1b53919c4ab8687
arVahedi <ar.vahedi357@gmail.com>
2022-05-07 04:41:23 +0430

    Initial commit
    Signed-off-by: arVahedi <ar.vahedi357@gmail.com>

---
+++ src/main/java/io/gl4dius/Gl4diusApplication.java
@@ -0,0 +1,13 @@
+package io.gl4dius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gl4diusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gl4diusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/gl4dius/Gl4diusApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gl4dius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gl4diusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
